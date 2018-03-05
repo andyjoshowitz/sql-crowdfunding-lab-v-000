487b8bd4c869b02259db64f9943637e1217180c0
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-03-05 20:36:22 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -3,14 +3,19 @@ CREATE TABLE projects (
   title TEXT,
   category TEXT,
   funding_goal INTEGER,
-  start_date ,
-  end_date
+  start_date TEXT,
+  end_date TEXT
 );
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
 );
 
 CREATE TABLE pledges (
   id INTEGER PRIMARY KEY,
+  amount INTEGER
+  project_id INTEGER,
+  user_id INTEGER
 );
